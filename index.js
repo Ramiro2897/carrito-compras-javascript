d5dcb20da7f0f67a2eb8c2702629cf5e2befd5f5
Ramiro <ramirodjaltor2016@gmail.com>
2024-02-02 20:49:22 -0500

    cambios en rama ramiro

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const productos = document.querySelectorAll('.producto');
-  const carritoHeader = document.getElementById('carrito-header');
+const carrito = document.querySelector('.carrito');
+const modalProductos = document.querySelector('.elementos-elegidos');
+const productos = document.querySelector('.productos');
+const cerrar = document.querySelector('.cerrar');
+const body = document.body;
 
-  let carrito = [];
+  carrito.addEventListener('click', function(){
+  modalProductos.style.display = 'flex';
+  productos.style.display = 'none';
+  document.body.style.overflow = "hidden";
+})
 
-  productos.forEach(producto => {
-    const botonAgregar = producto.querySelector('.pe p');
-    botonAgregar.addEventListener('click', () => {
-      const productId = producto.getAttribute('data-id');
-      carrito.push({
-        id: productId,
-        nombre: producto.querySelector('.info').textContent
-      });
-      actualizarCarritoHeader();
-    });
+cerrar.addEventListener('click', function(){
+  modalProductos.style.display = 'none';
+  productos.style.display = 'flex';
+  document.body.style.overflow = "";
+})
+
+// productos
+const datosProductos = [
+  { id: 1, nombre: "Reloj", precio: 2000 },
+  { id: 2, nombre: "Suéter", precio: 3500 }
+];
+
+function generarProductos() {
+  datosProductos.forEach(producto => {
+    const productoExistente = document.querySelector(`.producto[data-id="${producto.id}"]`);
+    if (productoExistente) {
+      productoExistente.querySelector('.info').textContent = producto.nombre;
+      productoExistente.querySelector('.p').textContent = producto.precio;
+    }
   });
+}
+generarProductos();
 
-  function actualizarCarritoHeader() {
-    carritoHeader.innerHTML = `Carrito: ${carrito.length} productos`;
-  }
+
+function agregarAlCarrito(nombre, precio) {
+  const carritoItemsContainer = document.querySelector('.carrito-items');
+
+  const carritoItem = document.createElement('div');
+  carritoItem.classList.add('carrito-item');
+  carritoItem.innerHTML = `<p>${nombre} - ${precio}</p>`;
+
+  carritoItemsContainer.appendChild(carritoItem);
+}
+
+const addCarritoIcons = document.querySelectorAll('.add-carrito');
+addCarritoIcons.forEach(icon => {
+  icon.addEventListener('click', () => {
+    const productoContainer = icon.closest('.producto');
+    const nombreProducto = productoContainer.querySelector('.info').textContent;
+    const precioProducto = productoContainer.querySelector('.p').textContent;
+
+    agregarAlCarrito(nombreProducto, precioProducto);
+  });
 });
+
 
